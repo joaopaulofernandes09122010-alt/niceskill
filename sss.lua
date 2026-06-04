b12ac0ffe81429d300dee4c0db8e70ef218c4b62
joaopaulofernandes09122010-alt <joaopaulofernandes09122010@gmail.com>
2026-06-04 17:08:44 -0300

    Update sss.lua

--- sss.lua
+++ sss.lua
@@ -1,19 +1,48 @@
 local HttpService = game:GetService("HttpService")
 
-local KEY = "ABC123" -- chave do comprador
+local WEBHOOK = "https://discord.com/api/webhooks/1512185960463597688/8gQmajWVdR7T-5BkXK8SMBME1-RMoPAieYlAdQvY0UXQbBTbVvpJu_lwxtrS30Uu49v7"
+local AUTORIZADOS = "https://raw.githubusercontent.com/joaopaulofernandes09122010-alt/niceskill/refs/heads/main/usuarios.json"
 
-local data = game:HttpGet(
-    "https://raw.githubusercontent.com/SeuUsuario/SeuRepositorio/main/usuarios.json"
-)
+local function GetHWID()
+    if gethwid then
+        return gethwid()
+    end
 
-local users = HttpService:JSONDecode(data)
-
-if not users[KEY] then
-    warn("Chave inválida")
-    return
+    return "HWID_INDISPONIVEL"
 end
 
-print("Autorizado!")
+local HWID = GetHWID()
 
--- Script principal
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Espectroxs/CLT-HUB/refs/heads/main/EspectralUnknown.lua"))()
+local ok, data = pcall(function()
+    return game:HttpGet(AUTORIZADOS)
+end)
+
+if ok then
+    local lista = HttpService:JSONDecode(data)
+
+    if lista[HWID] then
+        print("Autorizado!")
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Espectroxs/CLT-HUB/refs/heads/main/EspectralUnknown.lua"))()
+
+        return
+    end
+end
+
+local payload = {
+    content = string.format(
+        "Novo pedido de acesso\nHWID: `%s`",
+        HWID
+    )
+}
+
+request({
+    Url = WEBHOOK,
+    Method = "POST",
+    Headers = {
+        ["Content-Type"] = "application/json"
+    },
+    Body = HttpService:JSONEncode(payload)
+})
+
+warn("Seu HWID foi enviado para aprovação. Aguarde autorização.")
